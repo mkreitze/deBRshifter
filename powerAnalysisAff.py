38ed95b0a59c330567fa1eb7e0fcd279d88553d9
matth <matthewk2045@hotmail.ca>
2025-07-17 00:01:20 -0400

    reseting for Aff and non Aff deBRs

--- powerAnalysisAff.py
+++ powerAnalysisAff.py
@@ -1,8 +1,8 @@
-import deBRAffGen
+import deBRGen
 import deBRValidation
-import deBRVisualizeAff
-deBRValidation.VERBOSE = deBRAffGen.VERBOSE = False
-deBRVisualizeAff.ALLPRINT = False
+import deBRVisualize
+deBRValidation.VERBOSE = deBRGen.VERBOSE = False
+deBRVisualize.ALLPRINT = False
 
 
 #
@@ -12,7 +12,7 @@ deBRVisualizeAff.ALLPRINT = False
 
 ALPHABET = 2
 WINDOWDIM = (2,2) #Tuple of (length,width)
-APNUM = deBRAffGen.gen_apNum(ALPHABET,WINDOWDIM) # Number of aperiodic windows
+APNUM = deBRGen.gen_apNum(ALPHABET,WINDOWDIM) # Number of aperiodic windows
 ONLYSUCCESSFUL = True # True if we only want to pow = apN shifters
 ALLDATA = False # True if we want to see all shifters, not just successful ones
 WANTGRAPH = True # True if we want to graph the data
@@ -29,4 +29,4 @@ WANTHISTOGRAM = True # True if we want to graph the histogram of the data
 # CODE TO RUN
 
 print(f"Checking powers of shifters with A = {ALPHABET}, W = {WINDOWDIM}, apN = {APNUM}")
-successful = deBRVisualizeAff.test_deBR_powers(ALPHABET,WINDOWDIM,ALLDATA,ONLYSUCCESSFUL,WANTGRAPH,WANTHISTOGRAM)
+successful = deBRVisualize.test_deBR_powers(ALPHABET,WINDOWDIM,ALLDATA,ONLYSUCCESSFUL,WANTGRAPH,WANTHISTOGRAM)
