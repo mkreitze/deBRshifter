df3922660c756bbc463777f05b2918ed1d3ab7b3
matth <matthewk2045@hotmail.ca>
2025-11-13 00:45:32 -0500

    Got the sanity check working

--- deBRAffConstructor.py
+++ deBRAffConstructor.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import itertools as it
 import sympy as symp # used to determine factors of an integer
-import deBRConstructor # redundant code: gen_apn, gen_subcircs
+import deBRConstructor # redundant code: gen_apn, gen_subcircs, window_to_base10
 import typing as type # to make functions more clear
 from numpy.typing import NDArray 
 # ───────────────────────────────────────────────────────────
@@ -43,15 +43,69 @@ def gen_aff_free(A: int, W: type.Tuple[int, int]) -> NDArray[int]:
 # INFO (IS INEFFICIENT)
 # INPUT: W, combo (subcircs that make up shifter), allCircs (every sub-circ as a numpy array in a list)
 # OUTPUT: shifter (np array with composition in combo)
-def shifter_gen_combo(W: type.Tuple[int, int], combo: type.Tuple[int, ...],allCircs: type.List[NDArray[int]]) -> NDArray[int]:
+def shifter_gen_combo(W: type.Tuple[int, int], comboCirc: type.Tuple[int, ...],comboCol:  NDArray[int], allCircs: type.List[NDArray[int]]) -> NDArray[int]:
   shifter = basic_shifter(W)
   circRow = []
-  for subCirc in combo:
+  for subCirc in comboCirc:
     circRow.append(allCircs[subCirc])
   shifter[-W[0]-1:-1,0:-1] = np.concatenate(circRow,axis = 1) # np is
+  shifter[-W[0]-1:-1,-1:] = comboCol # affine column
   return(shifter)
 
+# INFO 
+# stuff
+def gen_all_npWindows(A: int,W: type.Tuple[int, ...])-> type.List[NDArray[int]]:
+  windows = it.product(range(A),repeat = W[0]*W[1])
+  npWindows = []
+  for window in windows:
+    temp = np.array(window,dtype=int).reshape(-1,1)
+    npWin = np.ones((W[0]*W[1]+1,1),dtype=int)
+    npWin[0:-1] = temp
+    npWindows.append(npWin)
+  return(npWindows)
 
+# INFO (can be done with casting, rolling and - but probably not efficient)
+# Literally just changed window_to_base10(tempWindow,A) to deBRConstructor.window_to_base10(tempWindow[:-1],A)
+def gen_cycles(shifter: NDArray[int],allNpWindows: type.List[NDArray[int]], A : int,W : type.Tuple[int,...]) -> type.List[type.List[NDArray[int]]]:
+  cycles = []
+  for window in allNpWindows:
+    if window[0] != -1:
+      cycle = [];base10Cycle=[]
+      cycle.append(window);base10Cycle.append(deBRConstructor.window_to_base10(window[:-1],A))
+      tempWindow = np.copy(window)
+      tempWindow = (shifter@tempWindow)%A
+      while not np.array_equal(tempWindow,window):
+        allNpWindows[deBRConstructor.window_to_base10(tempWindow[:-1],A)][0] = -1
+        cycle.append(tempWindow);base10Cycle.append(deBRConstructor.window_to_base10(tempWindow[:-1],A))
+        tempWindow = (shifter@tempWindow)%A
+      cycles.append([np.array(cycle).astype(int),np.array(base10Cycle)])
+  return(cycles)
+
+# INFO
+# Note, we do not actually _generate_ the tori here, just use the cycle
+def gen_tori(W: type.Tuple[int,...], cycle: NDArray[int]) -> NDArray[int]:
+  if len(cycle) < W[1]:
+    return(-1)
+  deBR = np.zeros((W[0],len(cycle)),dtype=int)
+  for i in range(len(cycle)):
+    temp = np.copy(cycle[i]) # to make sure it doesnt mess up the stored data
+    temp = temp[:-1].reshape((W[0],W[1]), order="F")
+    deBR[:,i] = temp[:,0] # copys each column of the torus
+  return(deBR)
+
+# INFO (true implies invert)
+def get_power(shifter: NDArray[int],A: int,W: type.Tuple[int,...], VERBOSE = False) -> int:
+  I = np.eye(W[0]*W[1]+1)
+  tempShifter = np.copy(shifter)
+  pow = 1
+  while not np.array_equal(tempShifter, I) and pow < A**(W[0]*W[1]):
+    if VERBOSE:
+      print(f"Shifter to the power of {pow}: \n {tempShifter}")
+    tempShifter = (shifter@tempShifter)%A
+    pow += 1
+  if VERBOSE:
+    print(f"Shifter to the power of {pow}: \n {tempShifter}")
+  return(pow)
 
 # INFO (true implies invert)
 def is_invert(shifter: NDArray[int],A: int) -> bool:
--- deBRSanityCheck.py
+++ deBRSanityCheck.py
@@ -6,7 +6,8 @@ import numpy as np
 W = (2,2) # defined as (length,width)
 A = 2 # alphabet size
 PRINTLINEAR = False
-PRINTAFFINE = True
+PRINTAFFINE = False
+PRINTAFFINESANITY = True
 
 print(f"System for {A};{W}")
 
@@ -71,18 +72,71 @@ if PRINTAFFINE:
 
     print(f"\n ALLONES AFFINE SHIFTER, COMPOSITION: {tuple(1 for i in range(W[1]))}")
     print("-----")
-    onesShifter = deBRAffConstructor.shifter_gen_combo(W,tuple(1 for i in range(W[1])),allSubCircs)
+    onesShifter = deBRAffConstructor.shifter_gen_combo(W,tuple(1 for i in range(W[1])),np.ones((W[1],1),dtype=int),allSubCircs)
+    print(onesShifter)
 
-# print(f"Affine Shifter with circ comp {tuple(1 for i in range(W[1]))}")
-# onesShifter = deBRAffConstructor.shifter_gen_combo(W,tuple(1 for i in range(W[1])),allSubCircs)
-# print(onesShifter)
-# print(f"Is this shifter invertible? {deBRConstructor.is_invert(onesShifter,A)}")
+    print(f"All generated cycles for allones shifter")
+    print("-----")
+    allWindows = deBRAffConstructor.gen_all_npWindows(A,W) # note windows are in base 10 order read bottom to top
+    onesCycles = deBRAffConstructor.gen_cycles(onesShifter,allWindows,A,W)
+    for cycle in onesCycles:
+        # prints each cycle as base 10 numbers in list 
+        print(f"For Cycle {cycle[1]}:")
+        # prints each cycle as windows (turn off if A,W large)
+        print(f"Windows: \n{cycle[0]}:")
+        # if associated tori are desired, can be generated here
+        tori = deBRAffConstructor.gen_tori(W,cycle[0])
+        print(f"Torus associated to cycle: \n {tori}")
+        print("-----")
 
-# print(f"Is shifter invertible? {deBRAffCo
-# print(f"All generated cycles for shifter")
+    print(f"Power of shifter {deBRAffConstructor.get_power(onesShifter,A,W,True)}")
+    print("-----")
 
-# print(f"Power of shifter")
+    print(f"Is shifter invertible? {deBRAffConstructor.is_invert(onesShifter,A)}")
+    print("-----")
 
+if PRINTAFFINESANITY:
+    print(f"\n \n \n FOR THE LINEAR AFFINE CASE \n \n \n")
 
-# print(f"\n \n \n FOR THE AFFINE CASE \n \n \n")
-# print(f"Generic affine shifter (-1 denotes free): \n {deBRAffConstructor.basic_shifter(W)}")
+    print(f"Generic affine shifter (-1 denotes free): \n {deBRAffConstructor.basic_shifter(W)}")
+
+    print(f"Number of aperodic windows (same as linear case): \n {deBRConstructor.get_apNum(A,W)}")
+
+    print(f"All sub-circs (same as linear case):")
+    allSubCircs = deBRConstructor.subcirc_gen_all(A,W)
+    i = 0
+    for subCirc in allSubCircs:
+        print(f"{subCirc}: Index {i}")
+        i+=1
+
+    print(f"Extra affine columns:")
+    affFrees = deBRAffConstructor.gen_aff_free(A,W)
+    i = 0
+    for affFree in affFrees:
+        print(f"{affFree}: Index {i}")
+        i+=1
+
+    print(f"\n ALLONES AFFINE SHIFTER, COMPOSITION: {tuple(1 for i in range(W[1]))}")
+    print("-----")
+    onesShifter = deBRAffConstructor.shifter_gen_combo(W,tuple(1 for i in range(W[1])),np.zeros((W[1],1),dtype=int),allSubCircs)
+    print(onesShifter)
+
+    print(f"All generated cycles for allones shifter")
+    print("-----")
+    allWindows = deBRAffConstructor.gen_all_npWindows(A,W) # note windows are in base 10 order read bottom to top
+    onesCycles = deBRAffConstructor.gen_cycles(onesShifter,allWindows,A,W)
+    for cycle in onesCycles:
+        # prints each cycle as base 10 numbers in list 
+        print(f"For Cycle {cycle[1]}:")
+        # prints each cycle as windows (turn off if A,W large)
+        print(f"Windows: \n{cycle[0]}:")
+        # if associated tori are desired, can be generated here
+        tori = deBRAffConstructor.gen_tori(W,cycle[0])
+        print(f"Torus associated to cycle: \n {tori}")
+        print("-----")
+
+    print(f"Power of shifter {deBRAffConstructor.get_power(onesShifter,A,W,True)}")
+    print("-----")
+
+    print(f"Is shifter invertible? {deBRAffConstructor.is_invert(onesShifter,A)}")
+    print("-----")
