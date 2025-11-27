55f1a5ea320c083a21b4ae45c53451d771e414b4
matth <matthewk2045@hotmail.ca>
2025-11-27 15:19:54 -0500

    hists updated

--- deBRSanityData.py
+++ deBRSanityData.py
@@ -9,7 +9,7 @@ import itertools as it
 # This code mainly just checks the 'all ones' version of a shifter for a specific alphabet size and windowsize. 
 # It checks the linear, and resulting affine case, for both scenarios. Helpful to make sure the code is working as intended
 
-W = (2,2) # defined as (length,width)
+W = (2,3) # defined as (length,width)
 A = 2 # alphabet size
 
 PRINTLINEAR = True
